8df571f73fb760d70ab47c169f25b5efbb128492
rkr2393 <rkr2393@gmail.com>
2016-10-28 01:29:47 -0400

    Scaled Hb level

--- pulse_oximeter/pulse_oximeter.ino
+++ pulse_oximeter/pulse_oximeter.ino
@@ -1,12 +1,26 @@
-void setup() {
-  // initialize the serial communication:
-  Serial.begin(9600);
+int sense = A0;
+float  x,y;
+
+void setup() {                
+  // initialize the digital pin as an output.
+  Serial.begin(9600); 
 }
 
+// the loop routine runs over and over again forever:
 void loop() {
-  // send the value of analog input 0:
-  Serial.println(analogRead(A0));
-  // wait a bit for the analog-to-digital converter
-  // to stabilize after the last reading:
-  delay(2);
+     float t =0;
+ x=0;
+ for (int i=0; i < 5; i++)
+ { 
+   t+=analogRead(sense); 
+ }
+ x=t/5; 
+ 
+y=map(x,0,1023,14,18);
+  Serial.print("your haemoglobin level is-");
+  Serial.print('\t');
+  Serial.println(y,3);
+  // Serial.print(analogRead(sense));
+  Serial.print('\n');
+delay(2000);
 }
